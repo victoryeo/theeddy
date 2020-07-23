9e9541b8294ef396caf1bc108b7496b30aec9214
Victor Yeo <victoryeo1974@gmail.com>
2020-07-23 22:26:47 +0800

    remove unused code

--- src/pages/Visit.js
+++ src/pages/Visit.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import Typography from '@material-ui/core/Typography';
 
@@ -54,10 +51,6 @@ class  Visit extends Component {
     );
   }
 }
-
-Visit.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
 export default (GoogleApiWrapper({
   apiKey: 'AIzaSyD6-FRm_114Pe1DbY8a5q87XUVo0Yr0JbY'
