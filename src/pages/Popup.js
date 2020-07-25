1d0f88d6a011636fe1e676a7181bb38aa3acf7de
Victor Yeo <victoryeo1974@gmail.com>
2020-07-25 16:24:35 +0800

    add bootstrap modal dialog

--- src/pages/Popup.js
+++ src/pages/Popup.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import Modal from 'react-bootstrap/Modal'
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalHeader from "react-bootstrap/ModalHeader";
+import ModalFooter from "react-bootstrap/ModalFooter";
+import ModalTitle from "react-bootstrap/ModalTitle";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class PopUp extends Component {
   handleClick = () => {
@@ -8,14 +15,16 @@ export default class PopUp extends Component {
 render() {
   //console.log(this.props)
   return (
-   <div >
-     <div className="modal_content">
-       <span className="close" onClick={this.handleClick}>&times;
-         <p>Search result</p>
-         <p>{this.props.string}</p>
-       </span>
-     </div>
-   </div>
+    <Modal show={true} size="sm">
+      <ModalHeader>
+        <ModalTitle>Search string</ModalTitle>
+      </ModalHeader>
+      <ModalBody>{this.props.string}</ModalBody>
+      <Modal.Footer>
+        <Button variant="primary" size="sm"  onClick={this.handleClick}>
+              Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
  }
 }
