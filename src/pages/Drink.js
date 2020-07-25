a5c84880e81c41a8bcc957bf77f916a2956e3b4a
Victor Yeo <victoryeo1974@gmail.com>
2020-07-25 20:10:21 +0800

    Grid list with titlebars

--- src/pages/Drink.js
+++ src/pages/Drink.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
 const styles = theme => ({
   root: {
@@ -24,13 +28,13 @@ const styles = theme => ({
 const tileData = [
   {
     img: "https://www.unicef.org/malaysia/sites/unicef.org.malaysia/files/styles/press_release_feature/public/beverages-carbonated-carbonated-drink-1282273.jpg",
-    title: "Drink",
-    author: "danfador"
+    title: "Soda",
+    author: "dan fador"
   },
   {
     img: "https://images.theconversation.com/files/194291/original/file-20171113-27595-ox08qm.jpg",
-    title: "Drink",
-    author: "jill111",
+    title: "Tequila",
+    author: "jill",
     cols: 2,
     featured: true
   },
@@ -39,12 +43,29 @@ const tileData = [
 function DrinkList(props) {
   const { classes } = props;
 
+  function handleClick(e) {
+    console.log('click')
+  };
+
   return (
     <div className={classes.root}>
       <GridList cellHeight={160} className={classes.gridList} cols={3}>
+      <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+        <ListSubheader component="div">July</ListSubheader>
+      </GridListTile>
         {tileData.map(tile => (
           <GridListTile key={tile.img} cols={tile.cols || 1}>
             <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>by: {tile.author}</span>}
+              actionIcon={
+                <IconButton aria-label={`info about ${tile.title}`}
+                className={classes.icon} onClick={handleClick} >
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
           </GridListTile>
         ))}
       </GridList>
